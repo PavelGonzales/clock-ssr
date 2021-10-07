8b6c69ab0fdb079c3772401900db360bb86105a3
Pavel Gonzales <p.gonzales@softpro.com>
2021-10-07 21:05:40 +0300

    SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,18 +3,43 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   head: {
     titleTemplate: '%s - clock-nuxt',
-    title: 'clock-nuxt',
+    title: 'Web Clock',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'telephone=no' },
+
+      { name: 'msapplication-TileColor', content: '#ffffff' },
+      { name: 'theme-color', content: '#ffffff' },
+      { name: 'description', content: 'Clock Online - exact time with night mode' },
+      { name: 'keywords', content: 'online clock, web clock, online time' },
+      { name: 'robots', content: 'index, follow' },
+      { property: 'vk:image"', content: 'img/og_vkontakte.png' },
+
+      { property: 'og:type', content: 'article' },
+      { property: 'og:title', content: 'Web Clock' },
+      { property: 'og:description', content: 'Clock Online - exact time with night mode' },
+      { property: 'og:image', content: 'img/og_facebook.png' },
+      { property: 'og:url', content: 'www.webclock.online' },
+      { property: 'og:site_name', content: 'www.webclock.online' },
+
+      { name: 'twitter:title', content: 'Web Clock' },
+      { name: 'twitter:description', content: 'Clock Online - exact time with night mode' },
+      { name: 'twitter:image', content: 'img/og_twitter.png' },
+      { name: 'twitter:site', content: 'www.webclock.online' },
+      { name: 'twitter:creator', content: '@gonzales_pavel' },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'apple-touch-icon', sizes: '180x180', href: 'img/icons/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'img/icons/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'img/icons/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: 'img/icons/safari-pinned-tab.svg', color: '#5bbad5' },
+      { rel: 'canonical', href: 'www.webclock.online' },
     ]
   },
 
